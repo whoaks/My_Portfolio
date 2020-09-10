9874cd78dc1967afcda27889763b101975c39ba2
whoaks <pandeypn.ap@gmail.com>
2020-09-10 17:44:26 +0530

    18:00

--- js/main.js
+++ js/main.js
@@ -10,18 +10,18 @@ const showMenu = (toggleId , navId) => {
 }
 showMenu('nav-toggle', 'nav-menu')
 
-const navLink = document.querySelectorAll('.nav_link')
+const navLink = document.querySelectorAll('.nav_link');
 
 function linkAction() {
     //Active-link
-    navLink.forEach(n => n.classList.remove('active'))
+    navLink.forEach(n => n.classList.remove('active'));
     this.classList.add('active')
 
     //Remove-link
-    const navMenu = document.getElementById('nav-menu')
+    const navMenu = document.getElementById('nav-menu');
     navMenu.classList.remove('show')
     
 }
 
-navLink.forEach(n => n.addEventListener('click',linkAction))
+navLink.forEach(n => n.addEventListener('click',linkAction));
 
