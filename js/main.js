d194b1999c014d412067ae154c04797db3e3d000
whoaks <pandeypn.ap@gmail.com>
2020-09-11 00:20:07 +0530

    11sep at 1am

--- js/main.js
+++ js/main.js
@@ -25,3 +25,27 @@ function linkAction() {
 
 navLink.forEach(n => n.addEventListener('click',linkAction));
 
+const sr = ScrollReveal({
+    origin: 'top' ,
+    distance: '80px' ,
+    duration: 2000,
+    reset: true 
+});
+
+sr.reveal('.home_title' , {})
+sr.reveal('.button' , {delay:200})
+sr.reveal('.home_img' , {delay:400})
+sr.reveal('.home_social-icon' , {interval: 200})
+
+sr.reveal('.about_img' , {})
+sr.reveal('.about_subtitle' , {delay:200})
+sr.reveal('.about_text' , {delay:400})
+
+sr.reveal('.skills_subtitle' , {})
+sr.reveal('.skills_text' , {delay:200})
+sr.reveal('.skills_data' , {interval: 200})
+sr.reveal('.skills_img' , {delay: 400})
+
+sr.reveal('.project_img' , {delay: 400})
+
+sr.reveal('.contact_input' , {delay: 400})
